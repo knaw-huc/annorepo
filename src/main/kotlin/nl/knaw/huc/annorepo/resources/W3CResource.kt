20f90188b2ae2ddf5e17cb7c117597fdf6624e63
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-04-14 13:30:59 +0200

    WIP: implement W3C resource

--- src/main/kotlin/nl/knaw/huc/annorepo/resources/W3CResource.kt
+++ src/main/kotlin/nl/knaw/huc/annorepo/resources/W3CResource.kt
@@ -3,18 +3,22 @@ package nl.knaw.huc.annorepo.resources
 import com.codahale.metrics.annotation.Timed
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import nl.knaw.huc.annorepo.AnnoRepoConfiguration
 import nl.knaw.huc.annorepo.api.ResourcePaths
 import org.jdbi.v3.core.Jdbi
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import org.slf4j.LoggerFactory
+import java.util.*
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
 
 @Api(ResourcePaths.W3C)
 @Path(ResourcePaths.W3C)
 @Produces(MediaType.APPLICATION_JSON)
-class W3CResource(jdbi: Jdbi) {
+class W3CResource(private val configuration: AnnoRepoConfiguration, val jdbi: Jdbi) {
+
+    val log = LoggerFactory.getLogger(javaClass)
 
     @ApiOperation(value = "Show link to the W3C Web Annotation Protocol")
     @Timed
@@ -26,15 +30,72 @@ class W3CResource(jdbi: Jdbi) {
     @ApiOperation(value = "Create an Annotation Container")
     @Timed
     @POST
-    fun createContainer() {
-
+    fun createContainer(): Response {
+        val name = UUID.randomUUID().toString()
+        log.info("create Container $name")
+        val uri = UriBuilder.fromUri(configuration.externalBaseUrl)
+            .path(ResourcePaths.W3C)
+            .path(name)
+            .build()
+        return Response.created(uri).build()
     }
 
     @ApiOperation(value = "Get an Annotation Container")
     @Timed
     @GET
-    fun readContainer() {
+    @Path("{containerName}")
+    fun readContainer(@PathParam("containerName") containerName: String) {
+        log.info("read Container $containerName")
+    }
 
+    @ApiOperation(value = "Delete an empty Annotation Container")
+    @Timed
+    @DELETE
+    @Path("{containerName}")
+    fun deleteContainer(@PathParam("containerName") containerName: String) {
+        log.info("delete Container $containerName")
+    }
+
+    @ApiOperation(value = "Create an Annotation")
+    @Timed
+    @POST
+    @Path("{containerName}")
+    fun createAnnotation(
+        @PathParam("containerName") containerName: String,
+        annotationJson: String
+    ): Response {
+        val name = UUID.randomUUID().toString()
+        log.info("create annotation $name in container $containerName")
+        log.info("annotation=\n$annotationJson")
+        val uri =
+            UriBuilder.fromUri(configuration.externalBaseUrl)
+                .path(ResourcePaths.W3C)
+                .path(containerName)
+                .path(name)
+                .build()
+        return Response.created(uri).build()
+    }
+
+    @ApiOperation(value = "Get an Annotation")
+    @Timed
+    @GET
+    @Path("{containerName}/{annotationName}")
+    fun readAnnotation(
+        @PathParam("containerName") containerName: String,
+        @PathParam("annotationName") annotationName: String
+    ) {
+        log.info("read annotation $annotationName in container $containerName")
+    }
+
+    @ApiOperation(value = "Delete an Annotation")
+    @Timed
+    @DELETE
+    @Path("{containerName}/{annotationName}")
+    fun deleteAnnotation(
+        @PathParam("containerName") containerName: String,
+        @PathParam("annotationName") annotationName: String
+    ) {
+        log.info("delete annotation $annotationName in container $containerName")
     }
 
 }
