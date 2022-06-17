41d9cd6e21fc211ae3e1a23a0d7fd2db2113b206
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-06-17 13:51:53 +0200

    add annotation update endpoint

--- src/main/kotlin/nl/knaw/huc/annorepo/resources/W3CResource.kt
+++ src/main/kotlin/nl/knaw/huc/annorepo/resources/W3CResource.kt
@@ -24,6 +24,7 @@ import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
+import javax.ws.rs.PUT
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
@@ -213,6 +214,43 @@ class W3CResource(
                 .tag(eTag)
                 .build()
         } else Response.status(Response.Status.NOT_FOUND).build()
+    }
+
+    @Operation(description = "Update an existing Annotation")
+    @Timed
+    @PUT
+    @Path("{containerName}/{annotationName}")
+    @Consumes(ANNOTATION_MEDIA_TYPE)
+    fun updateAnnotation(
+        @HeaderParam("slug") slug: String?,
+        @PathParam("containerName") containerName: String,
+        @PathParam("annotationName") annotationName: String,
+        annotationJson: String
+    ): Response {
+//        log.debug("annotation=\n$annotationJson")
+        val uri = uriFactory.annotationURL(containerName, annotationName)
+        val container = mdb.getCollection(containerName)
+        val existingAnnotationDocument = container.find(Document("annotation_name", annotationName)).first()
+            ?: return Response.status(Response.Status.NOT_FOUND).build()
+        val annotationDocument = Document.parse(annotationJson)
+        val doc = Document("annotation_name", annotationName).append("annotation", annotationDocument)
+        val r = container.updateOne(existingAnnotationDocument, annotationDocument).upsertedId?.asObjectId()?.value
+        val eTag = makeETag(annotationName)
+        val annotationData = AnnotationData(
+            r!!.timestamp.toLong(),
+            annotationName,
+            doc.getEmbedded(listOf("annotation"), Document::class.java).toJson(),
+            Date.from(Instant.now()),
+            Date.from(Instant.now())
+        )
+        val entity = withInsertedId(annotationData, containerName, annotationName)
+        return Response.ok(entity)
+            .allow("POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD")
+            .link("http://www.w3.org/ns/ldp#Resource", "type")
+            .link("http://www.w3.org/ns/ldp#Annotation", "type")
+            .tag(eTag)
+            .entity(entity)
+            .build()
     }
 
     @Operation(description = "Delete an Annotation")
