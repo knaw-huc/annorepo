cfb76efb23f4a06738bb8d14ba6dfe6755ac540c
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-07-04 13:17:32 +0200

    add github link to about info

--- src/main/kotlin/nl/knaw/huc/annorepo/api/AboutInfo.kt
+++ src/main/kotlin/nl/knaw/huc/annorepo/api/AboutInfo.kt
@@ -4,5 +4,6 @@ data class AboutInfo(
     val appName: String,
     val version: String,
     val startedAt: String,
-    val baseURI: String
+    val baseURI: String,
+    val source: String = "https://github.com/knaw-huc/annorepo"
 )
