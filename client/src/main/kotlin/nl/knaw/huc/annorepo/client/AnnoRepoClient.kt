51886a554ea06b2af9ebbb771bd4d0bb01136ed6
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-07-29 12:54:09 +0200

    add getAbout to client

--- client/src/main/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClient.kt
+++ client/src/main/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClient.kt
@@ -1,6 +1,15 @@
 package nl.knaw.huc.annorepo.client
 
 import java.net.URI
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.WebTarget
 
-class AnnoRepoClient(val serverURI: URI) {
+class AnnoRepoClient(serverURI: URI) {
+    private val webTarget: WebTarget = ClientBuilder.newClient()
+        .target(serverURI)
+
+    fun getAbout(): Map<String, Any> =
+        webTarget.path("about")
+            .request()
+            .get(Map::class.java) as Map<String, Any>
 }
--- client/src/test/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClientTest.kt
+++ client/src/test/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClientTest.kt
@@ -2,13 +2,19 @@ package nl.knaw.huc.annorepo.client
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import java.net.URI
 
-internal class AnnoRepoClientTest {
+class AnnoRepoClientTest {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val client = AnnoRepoClient(URI.create("http://localhost:9999"))
 
     @Test
-    fun `client should connect`() {
-        val client = AnnoRepoClient(URI.create("http://localhost:9000"))
+    fun `client should connect, and GET about should return a map with at least a version field`() {
         assertThat(client).isNotNull
+        val aboutInfo: Map<String, Any> = client.getAbout()
+        assertThat(aboutInfo).containsKey("version")
+        log.info("{}", aboutInfo)
     }
 }
