9bf84c0eb597ad3605ad7664641f17c71473428b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-08-16 14:05:19 +0200

    #9 - keep deprecated SearchResource for backwards compatibility.

--- server/src/main/kotlin/nl/knaw/huc/annorepo/AnnoRepoApplication.kt
+++ server/src/main/kotlin/nl/knaw/huc/annorepo/AnnoRepoApplication.kt
@@ -25,6 +25,7 @@ import nl.knaw.huc.annorepo.resources.AboutResource
 import nl.knaw.huc.annorepo.resources.BatchResource
 import nl.knaw.huc.annorepo.resources.HomePageResource
 import nl.knaw.huc.annorepo.resources.RuntimeExceptionMapper
+import nl.knaw.huc.annorepo.resources.SearchResource
 import nl.knaw.huc.annorepo.resources.ServiceResource
 import nl.knaw.huc.annorepo.resources.W3CResource
 import nl.knaw.huc.annorepo.service.LocalDateTimeSerializer
@@ -75,6 +76,7 @@ class AnnoRepoApplication : Application<AnnoRepoConfiguration?>() {
             register(HomePageResource())
             register(W3CResource(configuration, mongoClient))
             register(ServiceResource(configuration, mongoClient))
+            register(SearchResource(configuration, mongoClient))
             register(BatchResource(configuration, mongoClient))
 //            register(ListResource(configuration, mongoClient))
 
