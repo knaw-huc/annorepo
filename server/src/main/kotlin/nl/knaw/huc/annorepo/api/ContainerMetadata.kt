1b498dc888c4701d9b36f25c653570b13b407902
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-09-13 13:20:18 +0200

    add modification date to ContainerMetadata

--- server/src/main/kotlin/nl/knaw/huc/annorepo/api/ContainerMetadata.kt
+++ server/src/main/kotlin/nl/knaw/huc/annorepo/api/ContainerMetadata.kt
@@ -2,4 +2,9 @@ package nl.knaw.huc.annorepo.api
 
 import java.time.Instant
 
-data class ContainerMetadata(val name: String, val label: String, val createdAt: Instant = Instant.now())
+data class ContainerMetadata(
+    val name: String,
+    val label: String,
+    val createdAt: Instant = Instant.now(),
+    val modifiedAt: Instant = Instant.now()
+)
