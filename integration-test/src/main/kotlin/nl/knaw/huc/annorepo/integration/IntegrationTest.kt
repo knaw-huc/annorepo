fee92a55f3defce925d5df04be23cabbc7ddd7dc
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-09-19 10:38:01 +0200

    refactoring: extract runTest

--- integration-test/src/main/kotlin/nl/knaw/huc/annorepo/integration/IntegrationTest.kt
+++ integration-test/src/main/kotlin/nl/knaw/huc/annorepo/integration/IntegrationTest.kt
@@ -1,7 +1,9 @@
 package nl.knaw.huc.annorepo.integration
 
+import com.github.ajalt.mordant.rendering.TextColors.blue
 import com.github.ajalt.mordant.rendering.TextColors.green
 import com.github.ajalt.mordant.rendering.TextColors.red
+import com.github.ajalt.mordant.rendering.TextColors.yellow
 import com.github.ajalt.mordant.table.table
 import com.github.ajalt.mordant.terminal.Terminal
 import kotlinx.cli.ArgParser
@@ -20,40 +22,63 @@ class IntegrationTest {
 
         testResults["testAbout"] = client.testAbout(t)
         testResults["ContainerCreationAndDeletion"] = client.testContainerCreationAndDeletion(t)
+        testResults["container field count"] = client.testContainerFieldCount(t)
 
         t.printTable(testResults)
         t.println("done!")
     }
 
-    private fun AnnoRepoClient.testContainerCreationAndDeletion(
-        t: Terminal
-    ): Boolean {
-        val containerName = "testcontainer"
-        val r = createContainer(containerName)
-        t.println(green(r.toString()))
-        val deleteResult = deleteContainer(r.containerId, etag = r.eTag)
-        t.println(green(deleteResult.toString()))
-        return true
-    }
+    private fun AnnoRepoClient.testAbout(t: Terminal): Boolean =
+        runTest {
+            val about = getAbout()
+            t.println(green(about.toString()))
+            t.println(blue(about.version))
+        }
 
-    private fun AnnoRepoClient.testAbout(
-        t: Terminal
-    ): Boolean {
-        val about = getAbout()
-        t.println(green(about.toString()))
-        return true
-    }
+    private fun AnnoRepoClient.testContainerCreationAndDeletion(t: Terminal): Boolean =
+        runTest {
+            val containerName = "testcontainer"
+            val r = createContainer(containerName)
+            t.println(green(r.toString()))
+            val deleteResult = deleteContainer(r.containerId, etag = r.eTag)
+            t.println(green(deleteResult.toString()))
+        }
+
+    private fun AnnoRepoClient.testContainerFieldCount(t: Terminal): Boolean =
+        runTest {
+            val containerName = "testcontainer"
+            val r = createContainer(containerName)
+            t.println(green(r.toString()))
+            val fc = getFieldCount(containerName)
+            t.println(green(fc.toString()))
+            val deleteResult = deleteContainer(r.containerId, etag = r.eTag)
+            t.println(green(deleteResult.toString()))
+        }
 
     private fun Terminal.printTable(testResults: MutableMap<String, Boolean>) {
 //        val rows = testResults.map { name,result -> row(name, failureOrSuccess(result)) }
         println(table {
             header { row("Test", "Result") }
             body {
-                row("1", red("failure"))
-                row("2", green("success"))
+                testResults.forEach { (testName, success) -> row(yellow(testName), success.asResult()) }
             }
         })
     }
+
+    private fun runTest(test: () -> Unit): Boolean =
+        try {
+            test()
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+
+    private fun Boolean.asResult(): String =
+        when (this) {
+            true -> green("success")
+            false -> red("failure")
+        }
 
     companion object {
         @Throws(Exception::class)
@@ -70,3 +95,4 @@ class IntegrationTest {
         }
     }
 }
+
