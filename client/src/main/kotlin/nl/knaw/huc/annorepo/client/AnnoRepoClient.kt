77ca73ec792a2d70437adb7b02b841284decbd0f
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-10-11 15:59:42 +0200

    client refactoring: extract doGet/doPost/doPut/doDelete

--- client/src/main/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClient.kt
+++ client/src/main/kotlin/nl/knaw/huc/annorepo/client/AnnoRepoClient.kt
@@ -20,8 +20,6 @@ import nl.knaw.huc.annorepo.client.ARResponse.AnnoRepoResponse
 import nl.knaw.huc.annorepo.client.ARResponse.BatchUploadResponse
 import nl.knaw.huc.annorepo.client.RequestError.ConnectionError
 import nl.knaw.huc.annorepo.util.extractVersion
-import org.eclipse.jetty.http.HttpStatus
-import org.eclipse.jetty.http.HttpStatus.NO_CONTENT_204
 import org.glassfish.jersey.client.filter.EncodingFilter
 import org.glassfish.jersey.message.GZipEncoder
 import org.slf4j.LoggerFactory
@@ -65,73 +63,71 @@ class AnnoRepoClient(serverURI: URI, val apiKey: String? = null, private val use
     }
 
     fun getAbout(): Either<RequestError, AboutInfo> =
-        doRequest {
-            doGet(
-                request = webTarget.path(ABOUT)
-                    .request(),
-                responseHandlers = mapOf(
-                    Response.Status.OK to { response: Response ->
-                        val json = response.readEntity(String::class.java);
-                        Either.Right(oMapper.readValue(json))
-                    }
-                )
+        doGet(
+            request = webTarget.path(ABOUT)
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response: Response ->
+                    val json = response.readEntity(String::class.java);
+                    Either.Right(oMapper.readValue(json))
+                }
             )
-        }
+        )
 
     fun createContainer(
         preferredName: String? = null,
         label: String = "A container for web annotations"
-    ): Either<RequestError, AnnoRepoResponse> =
-        doRequest {
-            var request = webTarget.path(W3C).request()
-            if (preferredName != null) {
-                request = request.header("slug", preferredName)
-            }
-            doPost(
-                request = request,
-                entity = Entity.json(containerSpecs(label)),
-                responseHandlers = mapOf(
-                    Response.Status.CREATED to { response ->
-                        val location = location(response) ?: ""
-                        val containerId = extractContainerName(location)
-                        val etag = eTag(response) ?: ""
-                        Either.Right(
-                            AnnoRepoResponse(
-                                created = true,
-                                location = location,
-                                containerId = containerId,
-                                eTag = etag
-                            )
-                        )
-                    }
-                )
-            )
+    ): Either<RequestError, AnnoRepoResponse> {
+        var request = webTarget.path(W3C).request()
+        if (preferredName != null) {
+            request = request.header("slug", preferredName)
         }
+        return doPost(
+            request = request,
+            entity = Entity.json(containerSpecs(label)),
+            responseHandlers = mapOf(
+                Response.Status.CREATED to { response ->
+                    val location = location(response) ?: ""
+                    val containerId = extractContainerName(location)
+                    val eTag = eTag(response) ?: ""
+                    Either.Right(
+                        AnnoRepoResponse(
+                            created = true,
+                            location = location,
+                            containerId = containerId,
+                            eTag = eTag
+                        )
+                    )
+                }
+            )
+        )
+    }
 
     fun deleteContainer(containerName: String, eTag: String): Either<RequestError, Boolean> =
-        doRequest {
-            doDelete(
-                request = webTarget.path(W3C).path(containerName)
-                    .request()
-                    .header("if-match", eTag),
-                responseHandlers = mapOf(
-                    Response.Status.NO_CONTENT to { Either.Right(true) }
-                )
+        doDelete(
+            request = webTarget.path(W3C).path(containerName)
+                .request()
+                .header("if-match", eTag),
+            responseHandlers = mapOf(
+                Response.Status.NO_CONTENT to { Either.Right(true) }
             )
-        }
+        )
 
     fun createAnnotation(containerName: String, annotation: Map<String, Any>): Either<RequestError, AnnoRepoResponse> =
-        doRequest {
-            val request = webTarget.path(W3C).path(containerName).request()
-            val response = request.withHeaders()
-                .post(Entity.json(annotation))
-//        log.info("response={}", response)
-            val created = (response.status == HttpStatus.CREATED_201)
-            val location = location(response) ?: ""
-            val annotationName = extractAnnotationName(location)
-            val etag = eTag(response) ?: ""
-            Either.Right(AnnoRepoResponse(created, location, containerId = annotationName, eTag = etag))
-        }
+        doPost(
+            request = webTarget.path(W3C).path(containerName).request(),
+            entity = Entity.json(annotation),
+            responseHandlers = mapOf(
+                Response.Status.CREATED to { response ->
+                    val location = location(response) ?: ""
+                    val annotationName = extractAnnotationName(location)
+                    val eTag = eTag(response) ?: ""
+                    Either.Right(
+                        AnnoRepoResponse(true, location, containerId = annotationName, eTag = eTag)
+                    )
+                }
+            )
+        )
 
     fun updateAnnotation(
         containerName: String,
@@ -139,175 +135,175 @@ class AnnoRepoClient(serverURI: URI, val apiKey: String? = null, private val use
         eTag: String,
         annotation: Map<String, Any>
     ): Either<RequestError, AnnoRepoResponse> =
-        doRequest {
-            val request = webTarget.path(W3C).path(containerName).path(annotationName).request()
-            val response = request
-                .header("if-match", eTag)
-                .withHeaders()
-                .put(Entity.json(annotation))
-//        log.info("response={}", response)
-            val created = (response.status == HttpStatus.OK_200)
-            val location = location(response) ?: ""
-            val newEtag = eTag(response) ?: ""
-            Either.Right(AnnoRepoResponse(created, location, containerId = annotationName, eTag = newEtag))
-        }
+        doPut(
+            request = webTarget.path(W3C).path(containerName).path(annotationName)
+                .request()
+                .header("if-match", eTag),
+            entity = Entity.json(annotation),
+            responseHandlers = mapOf(
+                Response.Status.NO_CONTENT to { response ->
+                    val location = location(response) ?: ""
+                    val newEtag = eTag(response) ?: ""
+                    Either.Right(
+                        AnnoRepoResponse(false, location, containerId = annotationName, eTag = newEtag)
+                    )
+                }
+            )
+        )
 
     fun deleteAnnotation(containerName: String, annotationName: String, eTag: String): Either<RequestError, Boolean> =
-        doRequest {
-            val response = webTarget.path(W3C).path(containerName).path(annotationName)
+        doDelete(
+            request = webTarget.path(W3C).path(containerName).path(annotationName)
                 .request()
-                .header("if-match", eTag)
-                .withHeaders()
-                .delete()
-//        log.info("{}", response)
-            Either.Right(response.status == NO_CONTENT_204)
-        }
+                .header("if-match", eTag),
+            responseHandlers = mapOf(
+                Response.Status.NO_CONTENT to { Either.Right(true) }
+            )
+        )
 
     fun getFieldCount(containerName: String): Either<RequestError, Map<String, Int>> =
-        doRequest {
-            val json = webTarget.path(SERVICES).path(containerName).path(FIELDS)
-                .request()
-                .withHeaders()
-                .get(String::class.java)
-            Either.Right(oMapper.readValue(json))
-        }
+        doGet(
+            request = webTarget.path(SERVICES).path(containerName).path(FIELDS)
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response ->
+                    val json = response.readEntity(String::class.java)
+                    Either.Right(oMapper.readValue(json))
+                }
+            )
+        )
 
     fun batchUpload(
         containerName: String,
         annotations: List<Map<String, Any>>
     ): Either<RequestError, BatchUploadResponse> =
-        doRequest {
-            val request = webTarget.path(BATCH).path(containerName).path("annotations").request()
-            val response = request.withHeaders()
-                .post(Entity.json(annotations))
-//        log.info("response={}", response)
-            val entityJson: String =
-                response.readEntity(String::class.java)
-            val annotationData: List<AnnotationIdentifier> = oMapper.readValue(entityJson)
-            Either.Right(BatchUploadResponse(annotationData))
-        }
+        doPost(
+            request = webTarget.path(BATCH).path(containerName).path("annotations").request(),
+            entity = Entity.json(annotations),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response ->
+                    val entityJson: String =
+                        response.readEntity(String::class.java)
+                    val annotationData: List<AnnotationIdentifier> = oMapper.readValue(entityJson)
+                    Either.Right(BatchUploadResponse(annotationData))
+                }
+            )
+        )
 
     fun createQuery(containerName: String, query: Map<String, Any>): Either<RequestError, String> =
-        doRequest {
-            val response = webTarget.path(SERVICES).path(containerName).path("search")
-                .request()
-                .withHeaders()
-                .post(Entity.json(query))
-            log.info("response={}", response)
-            val location = response.location
-            Either.Right(location.rawPath.split("/").last())
-        }
+        doPost(
+            request = webTarget.path(SERVICES).path(containerName).path("search")
+                .request(),
+            entity = Entity.json(query),
+            responseHandlers = mapOf(
+                Response.Status.CREATED to { response ->
+                    val location = response.location
+                    Either.Right(location.rawPath.split("/").last())
+                }
+            )
+        )
 
     fun getQueryResult(containerName: String, queryId: String, page: Int): Either<RequestError, String> =
-        doRequest {
-            val response =
-                webTarget.path(SERVICES).path(containerName).path("search").path(queryId)
-                    .queryParam("page", page)
-                    .request()
-                    .withHeaders()
-                    .get()
-            Either.Right(response.readEntity(String::class.java))
-        }
+        doGet(
+            request = webTarget.path(SERVICES).path(containerName).path("search").path(queryId)
+                .queryParam("page", page)
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response ->
+                    Either.Right(response.readEntity(String::class.java))
+                }
+            )
+        )
 
     fun addIndex(containerName: String, fieldName: String, indexType: IndexType): Either<RequestError, Response> =
-        doRequest {
-            val entity = Entity.json(emptyMap<String, Any>())
-            val response =
-                webTarget.path(SERVICES).path(containerName).path("indexes").path(fieldName).path(indexType.name)
-                    .request()
-                    .withHeaders()
-                    .put(entity)
-            when (response.status) {
-                Response.Status.NO_CONTENT.statusCode -> Either.Right(response)
-                Response.Status.UNAUTHORIZED.statusCode -> unauthorizedResponse(response)
-                else -> unexpectedResponse(response)
-            }
-        }
+        doPut(
+            request = webTarget.path(SERVICES).path(containerName).path("indexes").path(fieldName).path(indexType.name)
+                .request(),
+            entity = Entity.json(emptyMap<String, Any>()),
+            responseHandlers = mapOf(
+                Response.Status.CREATED to { response -> Either.Right(response) }
+            )
+        )
 
     fun listIndexes(containerName: String): Either<RequestError, String> =
-        doRequest {
-            val response = webTarget.path(SERVICES).path(containerName).path("indexes")
-                .request()
-                .withHeaders()
-                .get()
-            when (response.status) {
-                Response.Status.OK.statusCode -> Either.Right(response.readEntity(String::class.java))
-                Response.Status.UNAUTHORIZED.statusCode -> unauthorizedResponse(response)
-                else -> unexpectedResponse(response)
-            }
-        }
+        doGet(
+            request = webTarget.path(SERVICES).path(containerName).path("indexes")
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response ->
+                    Either.Right(response.readEntity(String::class.java))
+                }
+            )
+        )
 
     fun deleteIndex(containerName: String, fieldName: String, indexType: IndexType): Either<RequestError, Boolean> =
-        doRequest {
-            val response =
-                webTarget.path(SERVICES).path(containerName).path("indexes").path(fieldName).path(indexType.name)
-                    .request()
-                    .withHeaders()
-                    .delete()
-            when (response.status) {
-                Response.Status.NO_CONTENT.statusCode -> Either.Right(true)
-                Response.Status.UNAUTHORIZED.statusCode -> unauthorizedResponse(response)
-                else -> unexpectedResponse(response)
-            }
-        }
+        doDelete(
+            request = webTarget.path(SERVICES).path(containerName).path("indexes").path(fieldName).path(indexType.name)
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.NO_CONTENT to { Either.Right(true) }
+            )
+        )
 
     fun getUsers(): Either<RequestError, List<UserEntry>> =
-        doRequest {
-            val response = webTarget.path(ADMIN).path(USERS)
-                .request()
-                .withHeaders()
-                .get()
-            when (response.status) {
-                Response.Status.OK.statusCode -> {
+        doGet(
+            request = webTarget.path(ADMIN).path(USERS)
+                .request(),
+            responseHandlers = mapOf(
+                Response.Status.OK to { response ->
                     val json = response.readEntity(String::class.java)
                     val userEntryList = oMapper.readValue(json, object : TypeReference<List<UserEntry>>() {})
                     Either.Right(userEntryList)
                 }
-
-                Response.Status.UNAUTHORIZED.statusCode -> unauthorizedResponse(response)
-
-                else -> unexpectedResponse(response)
-            }
-        }
+            )
+        )
 
     // private functions
     private fun <T> doGet(
         request: Invocation.Builder,
         responseHandlers: ResponseHandlerMap<T>
     ): Either<RequestError, T> =
-        request
-            .withHeaders()
-            .get()
-            .processResponseWith(responseHandlers)
+        doRequest {
+            request
+                .withHeaders()
+                .get()
+                .processResponseWith(responseHandlers)
+        }
 
     private fun <T> doPost(
         request: Invocation.Builder,
         entity: Entity<*>,
         responseHandlers: ResponseHandlerMap<T>
     ): Either<RequestError, T> =
-        request
-            .withHeaders()
-            .post(entity)
-            .processResponseWith(responseHandlers)
+        doRequest {
+            request
+                .withHeaders()
+                .post(entity)
+                .processResponseWith(responseHandlers)
+        }
 
     private fun <T> doPut(
         request: Invocation.Builder,
         entity: Entity<*>,
         responseHandlers: ResponseHandlerMap<T>
     ): Either<RequestError, T> =
-        request
-            .withHeaders()
-            .put(entity)
-            .processResponseWith(responseHandlers)
+        doRequest {
+            request
+                .withHeaders()
+                .put(entity)
+                .processResponseWith(responseHandlers)
+        }
 
     private fun <T> doDelete(
         request: Invocation.Builder,
         responseHandlers: ResponseHandlerMap<T>
     ): Either<RequestError, T> =
-        request
-            .withHeaders()
-            .delete()
-            .processResponseWith(responseHandlers)
+        doRequest {
+            request
+                .withHeaders()
+                .delete()
+                .processResponseWith(responseHandlers)
+        }
 
     private fun <T> Response.processResponseWith(
         responseHandlers: Map<Response.Status, (Response) -> Either<RequestError, T>>
@@ -315,7 +311,7 @@ class AnnoRepoClient(serverURI: URI, val apiKey: String? = null, private val use
         val handlerIdx = responseHandlers.entries.associate { it.key.statusCode to it.value }
         return when (status) {
             in handlerIdx.keys -> handlerIdx[status]!!.invoke(this)
-
+            Response.Status.UNAUTHORIZED.statusCode -> unauthorizedResponse(this)
             else -> unexpectedResponse(this)
         }
     }
