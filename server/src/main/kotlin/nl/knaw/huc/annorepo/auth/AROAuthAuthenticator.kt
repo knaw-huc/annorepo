62f4c304a863aec679807725c8ca361947b1f211
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-10-13 11:45:35 +0200

    Fix some maven warnings

--- server/src/main/kotlin/nl/knaw/huc/annorepo/auth/AROAuthAuthenticator.kt
+++ server/src/main/kotlin/nl/knaw/huc/annorepo/auth/AROAuthAuthenticator.kt
@@ -1,10 +1,6 @@
 package nl.knaw.huc.annorepo.auth
 
 import io.dropwizard.auth.Authenticator
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityScheme
-import nl.knaw.huc.annorepo.api.ARConst.SECURITY_SCHEME_NAME
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -13,9 +9,9 @@ class AROAuthAuthenticator(private val userDAO: UserDAO) : Authenticator<String,
     val log: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun authenticate(apiKey: String?): Optional<User> {
-        log.debug("Received api-key {}", apiKey)
+//        log.debug("Received api-key {}", apiKey)
         val userForApiKey = userDAO.userForApiKey(apiKey)
-        log.debug("api-key matches user {}", userForApiKey)
+//        log.debug("api-key matches user {}", userForApiKey)
         return Optional.ofNullable(userForApiKey)
     }
 
