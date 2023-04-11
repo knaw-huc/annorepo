2f4de6674254cfcad25813d8f086291d94925c09
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-04-11 15:24:33 +0200

    add test to check all AR_* env variables in conf.yml are defined in ARConstants

--- server/src/test/kotlin/nl/knaw/huc/annorepo/AnnoRepoConfigurationTest.kt
+++ server/src/test/kotlin/nl/knaw/huc/annorepo/AnnoRepoConfigurationTest.kt
@@ -1,8 +1,11 @@
 package nl.knaw.huc.annorepo
 
+import java.nio.file.Path
 import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import nl.knaw.huc.annorepo.api.ARConst
 import nl.knaw.huc.annorepo.config.AnnoRepoConfiguration
 
 class AnnoRepoConfigurationTest {
@@ -12,6 +15,16 @@ class AnnoRepoConfigurationTest {
     fun `get base uri`() {
         val c = AnnoRepoConfiguration()
         log.info(c.externalBaseUrl)
+    }
+
+    @Test
+    fun `all AR_ env variables in config yml are also in ARConst EnvironmentVariable`() {
+        val text = Path.of("../config.yml").toFile().readText()
+        val re = Regex("AR_[A-Z0-9_]+")
+        val definedEnvs = ARConst.EnvironmentVariable.values().map { it.name }.toSet()
+        val foundEnvs = re.findAll(text).map { it.value }
+        val undefinedEnvs = foundEnvs.filter { !definedEnvs.contains(it) }.toList()
+        assertThat(undefinedEnvs).isEmpty()
     }
 
 }
