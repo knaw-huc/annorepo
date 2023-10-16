09961c5da2fa8ee73dc80d093c0c77966494fb6b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-10-16 14:21:34 +0200

    setup protobuf in common; add test

--- common/src/test/kotlin/nl/knaw/huc/annorepo/api/ProtoBufTest.kt
+++ common/src/test/kotlin/nl/knaw/huc/annorepo/api/ProtoBufTest.kt
@@ -3,14 +3,20 @@ package nl.knaw.huc.annorepo.api
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.slf4j.LoggerFactory
-import nl.knaw.huc.annorepo.protocol.GreeterOuterClass.HelloReply
+import nl.knaw.huc.annorepo.grpc.AddAnnotationsRequest
+import nl.knaw.huc.annorepo.grpc.AddAnnotationsResponse
+import nl.knaw.huc.annorepo.grpc.AnnotationIdentifier
 
 internal class ProtoBufTest {
     private val log = LoggerFactory.getLogger(javaClass)
 
     @Test
-    fun `test HelloReply`() {
-        val helloReply = HelloReply.newBuilder().setMessage("Hello World").build()
-        assertThat(helloReply.message).isEqualTo("Hello World")
+    fun `test AnnotationUploadService`() {
+        val addAnnotationsRequest =
+            AddAnnotationsRequest.newBuilder().setContainerName("my-container").addAllAnnotations(listOf("")).build()
+        assertThat(addAnnotationsRequest.annotationsCount).isEqualTo(1)
+        val ai = AnnotationIdentifier.newBuilder().setId("").setEtag("").build()
+        val addAnnotationsResponse = AddAnnotationsResponse.newBuilder().addAnnotationIdentifiers(ai)
+        assertThat(addAnnotationsResponse.annotationIdentifiersCount).isEqualTo(1)
     }
 }
