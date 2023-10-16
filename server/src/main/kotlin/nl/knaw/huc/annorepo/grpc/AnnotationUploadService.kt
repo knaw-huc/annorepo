fd4599e60c84ee94f76c081a3d8cdbe149f45222
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-10-17 00:04:55 +0200

    setup AnnotationUploadService

--- server/src/main/kotlin/nl/knaw/huc/annorepo/grpc/AnnotationUploadService.kt
+++ server/src/main/kotlin/nl/knaw/huc/annorepo/grpc/AnnotationUploadService.kt
@@ -1,5 +1,14 @@
 package nl.knaw.huc.annorepo.grpc
 
-class AnnotationUploadService : AnnotationUploadServiceGrpcKt.AnnotationUploadServiceCoroutineImplBase() {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import org.slf4j.LoggerFactory
 
+class AnnotationUploadService : AnnotationUploadServiceGrpcKt.AnnotationUploadServiceCoroutineImplBase() {
+    val log = LoggerFactory.getLogger(AnnotationUploadService::class.java)
+    override fun addAnnotations(requests: Flow<AddAnnotationsRequest>): Flow<AddAnnotationsResponse> =
+        requests
+            .map { AddAnnotationsResponse.newBuilder().addAllAnnotationIdentifiers(listOf()).build() }
+            .flowOn(context)
 }
