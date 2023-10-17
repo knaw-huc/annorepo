006ba1050df17dd387a86cb3d1d74dfb6c8beba9
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-10-17 16:29:52 +0200

    build example response in service

--- server/src/main/kotlin/nl/knaw/huc/annorepo/grpc/AnnotationUploadService.kt
+++ server/src/main/kotlin/nl/knaw/huc/annorepo/grpc/AnnotationUploadService.kt
@@ -3,12 +3,26 @@ package nl.knaw.huc.annorepo.grpc
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import org.slf4j.LoggerFactory
 
 class AnnotationUploadService : AnnotationUploadServiceGrpcKt.AnnotationUploadServiceCoroutineImplBase() {
     val log = LoggerFactory.getLogger(AnnotationUploadService::class.java)
     override fun addAnnotations(requests: Flow<AddAnnotationsRequest>): Flow<AddAnnotationsResponse> =
         requests
-            .map { AddAnnotationsResponse.newBuilder().addAllAnnotationIdentifiers(listOf()).build() }
+            .onEach { log.info("request={}", it) }
+            .map {
+                AddAnnotationsResponse
+                    .newBuilder()
+                    .addAllAnnotationIdentifier(
+                        it.annotationList.map {
+                            annotationIdentifier {
+                                this.id = "x"
+                                this.etag = "etag"
+                            }
+                        }
+                    ).build()
+            }
+            .onEach { log.info("response={}", it) }
             .flowOn(context)
 }
