d595b13cc988e7d43cfbffd2d7cf39124a302003
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2024-12-03 17:56:47 +0100

    add 0.6 version of AnnotationPage

---
+++ common/src/main/kotlin/nl/knaw/huc/annorepo/api/OldAnnotationPage.kt
@@ -0,0 +1,19 @@
+package nl.knaw.huc.annorepo.api
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("@context", "id", "type", "partOf", "startIndex", "prev", "next", "items")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class OldAnnotationPage(
+    val id: String,
+    val partOf: String,
+    val startIndex: Int,
+    val items: List<WebAnnotationAsMap>,
+    @JsonProperty("@context") val context: List<String>? = null,
+    val prev: String? = null,
+    val next: String? = null
+) {
+    val type: String = "AnnotationPage"
+}
