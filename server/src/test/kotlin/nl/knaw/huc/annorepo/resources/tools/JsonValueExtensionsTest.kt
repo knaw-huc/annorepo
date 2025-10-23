b3b674241421240ac7d2202502f519a5f685b50c
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-10-23 13:54:01 +0200

    formatting

--- server/src/test/kotlin/nl/knaw/huc/annorepo/resources/tools/JsonValueExtensionsTest.kt
+++ server/src/test/kotlin/nl/knaw/huc/annorepo/resources/tools/JsonValueExtensionsTest.kt
@@ -3,6 +3,8 @@ package nl.knaw.huc.annorepo.resources.tools
 import java.io.StringReader
 import jakarta.json.Json
 import jakarta.json.JsonValue
+import kotlin.io.path.Path
+import kotlin.io.path.readText
 import org.junit.jupiter.api.Test
 import org.apache.logging.log4j.kotlin.logger
 import org.assertj.core.api.Assertions.assertThat
@@ -41,6 +43,24 @@ class JsonValueExtensionsTest {
         val arrayOfInts = map["arrayOfInts"]
         logger.info { arrayOfInts }
         assertThat(arrayOfInts.isNumberArray()).isTrue
+    }
+
+//    @Test
+    fun `read chunk`() {
+        val path = "/Users/bram/workspaces/annorepo/annorepo/chunk.json"
+        val json = Path(path).readText()
+        val value = Json.createReader(StringReader(json)).readValue()
+        if (value.valueType != JsonValue.ValueType.ARRAY) {
+            logger.error { "expected an array of web annotations, got a ${value.valueType}" }
+        }
+        val array = value.asJsonArray()
+        array.forEachIndexed { i, value ->
+            if (value.valueType != JsonValue.ValueType.OBJECT) {
+                logger.error { "item $i: expected a web annotation, got a ${value.valueType}" }
+            }
+        }
+        println(array.first())
+        println(array.last())
     }
 
     private fun Any?.isNumberArray() =
